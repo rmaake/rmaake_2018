c187ae4047eac197d6fd08e237b5ce2a91a165a2
Katleho <akhanye@student.wethinkcode.co.za>
2019-01-30 22:29:09 +0200

    changed environmnet values

--- ClientApp/src/environments/environment.prod.ts
+++ ClientApp/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:55089/api/'
+  // apiUrl: 'http://localhost:55089/api/'
+  apiUrl: 'https://h7a2mdqa3g.execute-api.us-east-1.amazonaws.com/Prod/api/'
 };
